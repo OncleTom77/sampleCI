9c54f5d426208351f46c043308adb60cc7394909
Thomas <thomasfouan@hotmail.fr>
2016-06-01 18:43:57 +0200

    update tests

--- test/index.specs.js
+++ test/index.specs.js
@@ -2,31 +2,31 @@ var request = require('supertest')
 var api = require('../index')
 
 describe('contacts', function() {
-	describe('GET /contacts', function() {
+	describe('GET /api/contacts', function() {
 		it('should return the list of contacts', function() {
-			return request(api).get('/contacts').send().expect(200)
+			return request(api).get('/api/contacts').send().expect(200)
 		})
 	})
 
-	describe('POST /contacts/:name', function() {
+	describe('POST /api/contacts/:name', function() {
 		it('should return the contact', function() {
-			return request(api).post('/contacts/toto').send().expect(200)
+			return request(api).post('/api/contacts/toto').send().expect(200)
 		})
 
 		it('should not be authorized to create a new contact with an already registrated name', function() {
-			return request(api).post('/contacts/exist').send().expect(403)
+			return request(api).post('/api/contacts/exist').send().expect(403)
 		})
 	})
 
-	describe('PUT /contacts/:name/:new', function() {
+	describe('PUT /api/contacts/:name/:new', function() {
 		it('should update contacts with the same name', function() {
-			return request(api).put('/contacts/toto/tata').send().expect(200)
+			return request(api).put('/api/contacts/toto/tata').send().expect(200)
 		})
 	})
 
-	describe('DELETE /contacts/:name', function() {
+	describe('DELETE /api/contacts/:name', function() {
 		it('should update contacts with the same name', function() {
-			return request(api).delete('/contacts/toto').send().expect(200)
+			return request(api).delete('/api/contacts/toto').send().expect(200)
 		})
 	})
 })
