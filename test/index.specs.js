974b8e8795d262af9fcd34cad0ace7769518d48e
Thomas <thomasfouan@hotmail.fr>
2016-06-01 16:27:53 +0200

    update tests

--- test/index.specs.js
+++ test/index.specs.js
@@ -1,8 +1,28 @@
 var request = require('supertest')
 var api = require('../index')
 
-describe('api', function() {
-	it('should return Hello World on GET /', function() {
-		return request(api).get('/').send().expect('Hello False')
+describe('contacts', function() {
+	describe('GET /contacts', function() {
+		it('should return the list of contacts', function() {
+			return request(api).get('/contacts').send().expect(200)
+		})
+	})
+
+	describe('POST /contacts/:name', function() {
+		it('should return the contact', function() {
+			return request(api).post('/contacts/toto').send().expect(200)
+		})
+	})
+
+	describe('PUT /contacts/:name/:new', function() {
+		it('should update contacts with the same name', function() {
+			return request(api).put('/contacts/toto/tata').send().expect(200)
+		})
+	})
+
+	describe('DELETE /contacts/:name', function() {
+		it('should update contacts with the same name', function() {
+			return request(api).delete('/contacts/toto').send().expect(200)
+		})
 	})
 })
